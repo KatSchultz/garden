764a49f2c0843674d819ae6de06570fe5ef3b823
KatSchultz <ks.katieschultz@gmail.com>
2024-01-03 10:51:33 -0500

    routing and header styling

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import "./App.css";
+import Home from "../../pages/Home";
 
 const App = () => {
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
+        <div className="app__contents">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 };
